049605b8d5c28f439bf1ddf3d7d9c479b3ba480d
MdMahaboobkhan <khan.m.mahaboob@gmail.com>
2020-12-20 18:56:57 -0500

    loading home screen data from mongodb

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -1,22 +1,42 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 
 
 
 const Home =()=>{
+    const [data,setData] = useState([])
+    useEffect(()=>{
+        fetch('/allpost',{
+            headers:{
+                "Authorization":"Bearer "+localStorage.getItem("jwt")
+            }
+        }).then(res=>res.json())
+        .then(result=>{
+            console.log(data)
+            setData(result.posts)
+        })
+
+    },[])
     return (
         <div className="home">
-            <div className="card home-card">
-                <h5>Casper</h5>
-                <div className="card-image">
-                    <img src="https://images.unsplash.com/photo-1461301214746-1e109215d6d3?ixid=MXwxMjA3fDB8MHxzZWFyY2h8N3x8d2FsbHBhcGVyfGVufDB8MHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"/>
-                </div>
-                <div className="card-content">
-                    <i className="material-icons" style={{color:"red"}}>favorite</i>
-                    <h6>title</h6>
-                    <p>My first post</p>
-                    <input type="text" placeholder="add a comment"/>
-                </div>
-            </div>
+            {
+                data.map(item=>{
+                    return(
+                        <div className="card home-card" key={item._id}>
+                            <h5>{item.postedBy.name}</h5>
+                            <div className="card-image">
+                                <img src={item.photo}/>
+                            </div>
+                            <div className="card-content">
+                                <i className="material-icons" style={{color:"red"}}>favorite</i>
+                                <h6>{item.title}</h6>
+                                <p>{item.body}</p>
+                                <input type="text" placeholder="add a comment"/>
+                            </div>
+                        </div>
+                    )
+                })
+            }
+            
 
         </div>
     )
