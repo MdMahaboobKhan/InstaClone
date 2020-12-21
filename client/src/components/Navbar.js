378e1aef07d853da1dbca088a071587bc20f9efc
MdMahaboobkhan <khan.m.mahaboob@gmail.com>
2020-12-20 19:06:13 -0500

    adding logout feature

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,14 +1,27 @@
 import React,{useContext} from 'react'
 import {UserContext} from '../App'
-import {Link} from 'react-router-dom'
+import {Link, useHistory} from 'react-router-dom'
 
 const NavBar = ()=>{
+    const history = useHistory()
     const {state,dispatch} = useContext(UserContext)
     const renderList = ()=>{
         if (state){
             return [
                 <li><Link to="/profile">Profile</Link></li>,
-                <li><Link to="/create">Create Post</Link></li>
+                <li><Link to="/create">Create Post</Link></li>,
+                <li>
+                <button className="btn waves-effect waves-light #c62828 red darken-3"
+                onClick = {()=>{
+                    localStorage.clear()
+                    dispatch({type:"CLEAR"})
+                    history.push('/signin')
+                }}
+                >
+                    Logout
+                </button>
+                </li>
+
             ]
         }else{
             return [
